5f3a75cc012f1f8a2c0f4fa2ace2d7540c8b44bc
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-10-11 09:55:25 +0800

    lastipdate

--- lib/pages/create_todo.dart
+++ lib/pages/create_todo.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/blocs/blocs.dart';
-import 'package:todo_app/cubits/todo_list/todo_list_cubit.dart';
 
 class CreateTodo extends StatefulWidget {
   const CreateTodo({super.key});
